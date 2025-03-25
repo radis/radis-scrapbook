3a40c52e76411445258a2e6e3f067f676615d1eb
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2025-03-25 15:50:47 +0100

    Update partial_decompression.py

--- partial_bz2/partial_decompression.py
+++ partial_bz2/partial_decompression.py
@@ -10,7 +10,6 @@ from mypath import fpath
 import bz2
 
 fname = fpath + '02_HITEMP2024.par.bz2'
-sname_i = 'db/db_{:05d}.txt.bz2'
 sname = 'db/db_subset.txt'
 
 offsets, pads = np.load('offset_arr.npy')
@@ -21,25 +20,6 @@ def merge_bytes(byte1, byte2, pad):
     mask = 0xff << pad
     return int((byte1 & mask) | (byte2 & (~mask))).to_bytes(1)
     
-
-# def get_block_magic_shifted(pad):
-#     # blk_hdr_int = int(0x314159265359 << pad)
-#     # return blk_hdr_int.to_bytes(7)[0]
-#     return 24 >> (7-pad)
-
-
-# ## Iterate over pad=0..7 to extract the bytestrings and copy them below
-# for pad in range(8):
-#     with open('bit_shifts/bit_shift_{:d}.bz2'.format(pad), 'rb') as fr:
-#         pad_buf = fr.read()[10:-10] #skip stream header and footer magic+CRC
-        
-#         # add the shifted bits of the block magic from the data buf:
-#         temp_buf = get_block_magic_shifted(pad)
-#         temp_buf = merge_bytes(pad_buf[-1], temp_buf, pad)
-            
-#     print('\t'+str(pad_buf[:-1]+temp_buf)+',')
-# sys.exit()
-
 
 stream_header = b'BZh9'
 block_magic = b'1AY&SY'
@@ -53,37 +33,18 @@ pad_bufs = [
 	b'|\x80\xf4\xd9\x00\x00\x03Z\x00\x00\x10\x0c\x00\x01\x00\x00\n \x00"\x18h0\x02\xcf)\x8c',
 	b'\xc9\xec\x1b[\x00\x00\x03Z\x00\x00\x10\x0e\x00\x00\x80\x00\n \x001\x0c\x01\r1\xa8Y0\xa7\x98',
     ]
-eos_marker = 0x177245385090
 
 
-class StreamCRC:
-    def __init__(self):
-        self.reset()
-        
-    def reset(self):
-        self.strmCRC = 0
-    
-    def update(self, blkCRC):
-        if type(blkCRC) == type(b''):
-            blkCRC = int.from_bytes(blkCRC)
-        blkCRC &= 0xffffffff
-        self.strmCRC = blkCRC ^ ((self.strmCRC<<1) | (self.strmCRC>>31)) 
-    
-    def result(self):
-        return self.strmCRC & 0xffffffff
-
-
-crc_obj = StreamCRC()
-decomp = None
+decomp = bz2.BZ2Decompressor()
+init = None
 with open(fname, 'rb') as fr, open(sname, 'wb') as fw:
     for i in [1,2,3]:
 
         print(i, offsets[i], pads[i])
         
         # write alignment block:
-        if decomp is None:
-            decomp = bz2.BZ2Decompressor()
-            dummy = decomp.decompress(stream_header + block_magic + pad_bufs[pads[i]])
+        if init is None:
+            init = decomp.decompress(stream_header + block_magic + pad_bufs[pads[i]])
             fr.seek(offsets[i])
 
         # write data block:
